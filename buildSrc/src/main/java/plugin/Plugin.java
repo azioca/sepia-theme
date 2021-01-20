8cb38d4f2f05a8b7643675cca75e238bc2002702
adam.wojszczyk <adam.wojszczyk@gmail.com>
2021-01-20 18:57:07 +0100

    Add Theme suffix to plugin id

--- buildSrc/src/main/java/plugin/Plugin.java
+++ buildSrc/src/main/java/plugin/Plugin.java
@@ -11,12 +11,14 @@ public class Plugin {
 	private final String name = "Gruvbox Light Hard Black";
 	private final Palette palette = new GruvBoxPallete();
 	private final Color background = palette.sepia().brighter().brighter().brighter();
+	private final Color foreground = palette.black().darker().darker();
+
+	@JsonProperty public String id() { return "com.github.adamwojszczyk." + toCamelCase() + "Theme"; }
+	@JsonProperty public String name() { return name + " Theme"; }
 	@JsonProperty public String email() { return "adam.wojszczyk@gmail.com"; }
 	@JsonProperty public String vendor() { return "Adam Wojszczyk"; }
 	@JsonProperty public String themeProviderId() { return "f20d8fb3-da86-46ac-8621-661b16f0d135"; }
-	private final Color foreground = palette.black().darker().darker();
-	@JsonProperty public String id() { return "com.github.adamwojszczyk." + toCamelCase(); }
-	@JsonProperty public String name() { return name + " Theme"; }
+
 	public Theme theme() {
 		return new Theme(palette, background, foreground, name + " Theme", vendor());
 	}
