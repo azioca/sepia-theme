3278d70afa5a5e879ca3420abc37625885c28686
adam.wojszczyk <adam.wojszczyk@gmail.com>
2021-01-26 01:25:28 +0100

    Make Scrollbar wider but one tone brighter

--- buildSrc/src/main/java/plugin/Scrollbar.java
+++ buildSrc/src/main/java/plugin/Scrollbar.java
@@ -10,8 +10,8 @@ public class Scrollbar {
 
 	public Color trackColor() { return trackColor; }
 	public Color thumbColor() { return trackColor().darker(3).opacity(0.4); }
-	public Color thumbBorderColor() { return thumbColor().opacity(0); }
+	public Color thumbBorderColor() { return thumbColor(); }
 	public Color hoverTrackColor() { return trackColor(); }
 	public Color hoverThumbColor() { return thumbColor().opacity(0.65); }
-	public Color hoverThumbBorderColor() { return hoverThumbColor().opacity(0); }
+	public Color hoverThumbBorderColor() { return hoverThumbColor(); }
 }
